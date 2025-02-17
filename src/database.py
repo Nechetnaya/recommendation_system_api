0d2271f9ca1e4f16c6d9edf72c9cd66027dc130e
Irina Nechetnaya <nechetnaya@gmail.com>
2025-02-17 11:22:16 +0300

    update connection to env.var.conn

--- src/database.py
+++ src/database.py
@@ -1,10 +1,22 @@
+import os # work with env var
+from dotenv import load_dotenv # load env var
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL = "postgresql://robot-startml-ro:pheiph0hahj1Vaif@postgres.lab.karpov.courses:6432/startml"
+# connection with environment variables
+load_dotenv()
 
+user = os.environ.get('POSTGRES_USER')
+password = os.environ.get('POSTGRES_PASSWORD')
+host = os.environ.get('POSTGRES_HOST')
+port = os.environ.get('POSTGRES_PORT')
+database = os.environ.get('POSTGRES_DATABASE')
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{user}:{password}@{host}:{port}/{database}"
+
+# engine with SQLAlchemy
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
